f02b10ced6c2eb5ed12e9bd95081878504132812
Evanderss <evanderramirez150@gmail.com>
2022-07-28 20:42:18 -0500

    Creando la aplicación generator, home y about

--- generator/views.py
+++ generator/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+# from django.http import HttpResponse
 
 # Create your views here.
+def home(request):
+    return render(request, "generator/home.html")
+
 def about(request):
-    return HttpResponse("Hello world")
+    # return HttpResponse("Hello world")
+    return render(request, "generator/about.html")
